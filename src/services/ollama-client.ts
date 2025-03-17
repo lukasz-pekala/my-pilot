c42ea9aeb628d6b49bfc8b280b224eb1ed4d3394
Łukasz Pękala <lukasz-pekala@users.noreply.github.com>
2025-03-17 20:11:57 +0100

    refactor OllamaClient interface to improve chat method typing and add ps method

--- src/services/ollama-client.ts
+++ src/services/ollama-client.ts
@@ -1,8 +1,13 @@
-import ollama, { ListResponse } from 'ollama';
+import ollama, { ChatRequest, ListResponse } from "ollama";
 
 export interface OllamaClient {
   list(): Promise<ListResponse>;
-  chat(params: any): Promise<AsyncIterable<any>>;
+  chat(
+    request: ChatRequest & {
+      stream: true;
+    }
+  ): Promise<AsyncIterable<any>>;
+  ps(): Promise<ListResponse>;
 }
 
 export class DefaultOllamaClient implements OllamaClient {
@@ -10,7 +15,15 @@ export class DefaultOllamaClient implements OllamaClient {
     return await ollama.list();
   }
 
-  async chat(params: any): Promise<AsyncIterable<any>> {
-    return await ollama.chat(params);
+  async chat(
+    request: ChatRequest & {
+      stream: true;
+    }
+  ): Promise<AsyncIterable<any>> {
+    return await ollama.chat(request);
+  }
+
+  async ps() {
+    return await ollama.ps();
   }
 }
